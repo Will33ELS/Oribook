652c9366b55a972c6977e237f5be4590cc164562
William AVENTIN <foy33@hotmail.fr>
2021-01-02 18:09:05 +0100

    Envoie du formulaire à l'api

--- js/app.js
+++ js/app.js
@@ -89,12 +89,10 @@ const clearBasket = () => {
     localStorage.removeItem("basket");
     //SUPPRESSION DU CONTENU DU PANIER
     const tbody = document.getElementById("panier");
-    tbody.remove();
+    if(tbody != null)
+        tbody.remove();
     $("#panier-total").html(0);
-    const panierAlert = document.getElementById("panier-alert");
-    const alert = document.createElement("div");
-    $(alert).addClass("alert alert-info").attr("role", "alert").html("<i class='fas fa-info-circle'></i> Votre panier est vide");
-    $(panierAlert).append(alert);
+    sendAlert("info","<i class='fas fa-info-circle'></i> Votre panier est vide");
 }
 
 /* QUANTITE DUN MEME ARTICLE DANS LE PANIER */
@@ -113,10 +111,8 @@ const countArticleInBasket = (item_id) => {
 const showBasket = () => {
     const basket = localStorage.getItem('basket') == null ? [] : JSON.parse(localStorage.getItem('basket'));
     if(basket.length == 0){
-        const panierAlert = document.getElementById("panier-alert");
-        const alert = document.createElement("div");
-        $(alert).addClass("alert alert-info").attr("role", "alert").html("<i class='fas fa-info-circle'></i> Votre panier est vide");
-        $(panierAlert).append(alert);
+        sendAlert("info", "<i class='fas fa-info-circle'></i> Votre panier est vide");
+        document.getElementById("button_command").setAttribute("disabled", true); //Le panier est vide, on désactive le bouton commander
     }else{
         const tbody = document.getElementById("panier");
         let total = 0;
@@ -146,6 +142,68 @@ const showBasket = () => {
         });
     }
 }
+
+const validateFormulaire = () => {
+    if(localStorage.getItem('basket') == null ){
+        sendAlert("info", "<i class='fas fa-info-circle'></i> Votre panier est vide");
+    }else{
+        const basket = JSON.parse(localStorage.getItem('basket'));
+        const productsID = [];
+        basket.forEach(item => productsID.push(item.id));
+        const lastNameInput = document.getElementById("nom").value; //Input Nom
+        const firstNameInput = document.getElementById("prenom").value; //Input Prénom
+        const emailInput = document.getElementById("email").value; //Input Email
+        const addressInput = document.getElementById("address").value; //Input Adresse
+        const cityInput = document.getElementById("city").value; //Input Ville
+        const codePostalInput = document.getElementById("code_postal").value; //Input Code postal
+        //VERIFICATION QUE LES VALEURS NE SONT PAS VIDE
+        if(lastNameInput.length > 0 && firstNameInput.length > 0 && emailInput.length > 0 && addressInput.length > 0 && cityInput.length > 0 && codePostalInput.length > 0){
+            //ENVOIE DE LA REQUETE A L'API
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                //RECUPERATION DE LA REPONSE DE L'API
+                if (this.status == 201) {
+                    var data = JSON.parse(this.responseText);
+                    //TODO TRAITEMENT DE LA REPONSE
+                    console.log(data);
+
+                }else if(this.status >= 400 && this.status < 600){ //L'API A RETOURNE UNE ERREUR
+                    sendAlert("danger", "Une erreur a eu lieu lors de l'envoie du formulaire.")
+                }
+            };
+            xhr.open("POST", hostAPI+"/api/teddies/order", false);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            //DEFINITION DU BODY
+            xhr.send(JSON.stringify({
+                    "contact": {
+                        "firstName": firstNameInput,
+                        "lastName": lastNameInput,
+                        "address": addressInput,
+                        "city": cityInput,
+                        "email": emailInput
+                    },
+                    "products": productsID
+                }
+            ));
+        }else{
+            sendAlert("danger", "<i class=\"fas fa-exclamation-triangle\"></i> Tous les champs sont requis.");
+        }
+    }
+};
+
+/* Affichage d'alerte sur la page Panier.html */
+const sendAlert = (alertType, message) => {
+    if(alertType !== "primary" && alertType !== "secondary" && alertType !== "success" && alertType !== "danger" && alertType !== "warning" && alertType !== "info" && alertType !== "light" && alertType !== "dark"){
+        console.error("Le type d'alerte renseigné n'est pas correct.")
+    }else {
+        const panierAlert = document.getElementById("panier-alert");
+        if(panierAlert.childNodes.length > 0)
+            panierAlert.removeChild(panierAlert.childNodes[0]);
+        const alert = document.createElement("div");
+        $(alert).addClass("alert alert-" + alertType).attr("role", "alert").html(message);
+        $(panierAlert).append(alert);
+    }
+};
 
 /* CLASSE QUI CORRESPOND A L'ARTICLE */
 class Item{
