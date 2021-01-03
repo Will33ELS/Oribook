9511b7ba93bccec5d563df7ae91ddb99fcb46b94
William AVENTIN <foy33@hotmail.fr>
2021-01-03 20:15:45 +0100

    Utilisation des promises pour la connexion à l'API

--- js/app.js
+++ js/app.js
@@ -3,66 +3,67 @@
 const hostAPI = "http://localhost:3000"; // A MODIFIER LORS DE L'INSTALLATION
 
 /* AFFICHAGES DES ITEMS DEPUIS L'API*/
-const showItems = () => {
-    $.get(hostAPI+"/api/teddies").done(function(data){
-        let items = [];
-        data.forEach(item => items.push({
-            "id": item._id,
-            "name": item.name,
-            "price": item.price,
-            "description": item.description,
-            "imageUrl": item.imageUrl
-        }));
-        items.forEach(item => {
-            console.log(item);
-            const card = $(document.createElement("div"));
-            card.addClass("m-2 card").css("width", "18rem");
-            const img = $(document.createElement("img"));
-            img.addClass("card-img-top").attr("src", item.imageUrl).attr("alt", item.name).appendTo(card);
-            const cardBody = $(document.createElement("div"));
-            cardBody.addClass("card-body").appendTo(card);
-            const cardTitle = $(document.createElement("h5"));
-            cardTitle.addClass("card-title").html(item.name).appendTo(cardBody);
-            const cardDescription = $(document.createElement("p"));
-            cardDescription.addClass("card-text").html(item.description).appendTo(cardBody);
-            const cardPrice = $(document.createElement("p"));
-            cardPrice.addClass("card-text").html(item.price+"€").appendTo(cardBody);
-            const cardLink = $(document.createElement("a"));
-            cardLink.addClass("btn btn-primary").html("Voir le produit").attr("href", "produit.html?id="+item.id).appendTo(cardBody);
-
-            $("#items").append(card);
-        });
-
+const showItems = async () => {
+    fetch(`${hostAPI}/api/teddies/`).then(response => {
+        const jsonReponse = response.json(); //RECUPERATION DU JSON
+        jsonReponse.then(json => {
+            const data = Object.values(json); //CONVERSION DU JSON EN ARRAY
+            const items = data.map(({_id, name, price, description, imageUrl}) => ({
+                _id, name, price, description, imageUrl
+            }));
+            items.forEach(item => {
+                const card = $(document.createElement("div"));
+                card.addClass("m-2 card").css("width", "18rem");
+                const img = $(document.createElement("img"));
+                img.addClass("card-img-top").attr("src", item.imageUrl).attr("alt", item.name).appendTo(card);
+                const cardBody = $(document.createElement("div"));
+                cardBody.addClass("card-body").appendTo(card);
+                const cardTitle = $(document.createElement("h5"));
+                cardTitle.addClass("card-title").html(item.name).appendTo(cardBody);
+                const cardDescription = $(document.createElement("p"));
+                cardDescription.addClass("card-text").html(item.description).appendTo(cardBody);
+                const cardPrice = $(document.createElement("p"));
+                cardPrice.addClass("card-text").html(item.price + "€").appendTo(cardBody);
+                const cardLink = $(document.createElement("a"));
+                cardLink.addClass("btn btn-primary").html("Voir le produit").attr("href", "produit.html?id=" + item._id).appendTo(cardBody);
+                $("#items").append(card);
+            });
+        })
     }).catch(function (err){
         sendAlert("danger", "<i class=\"fas fa-exclamation-triangle\"></i> Un incident lors de la connexion à l'API a eu lieu.");
     });
-};
+}
+
 
 /* AFFICHAGE DES INFORMATIONS DE L'ARTICLE */
-const showItem = (item_id) => {
-    $.get(hostAPI+"/api/teddies").done(function(data){
-        let item = null;
-        //RECUPERATION DE L'ARTICLE A PARTIR DE L'ID FOURNIS
-        data.forEach(itemData => {
-            if(item_id == itemData._id)
-                item = itemData;
-        });
+const showItem = async (item_id) => {
+    fetch(hostAPI + "/api/teddies").then(response => {
+        const responseJson = response.json(); //RECUPERATION DU JSON
+        responseJson.then(json => {
+            const data = Object.values(json); //CONVERSION DU JSON EN ARRAY
+            let item = null;
+            //RECUPERATION DE L'ARTICLE A PARTIR DE L'ID FOURNIS
+            data.forEach(itemData => {
+                if (item_id == itemData._id)
+                    item = itemData;
+            });
 
-        if(item == null) //AUCUN ARTICLE TROUVE SUR L'ID FOURNIS, REDIRECTION DE L'UTILISATEUR
-            window.location.href = "index.html";
-        else{
-            //ARTICLE TROUVE, INSERTION DES INFORMATIONS
-            $("#product-name").html(item.name);
-            $("#product-description").html(item.description);
-            $("#product-price").html(item.price);
-            const select = $("#product-options");
-            item.colors.forEach(color =>  select.append(new Option(color, color)));
-            const img = $(document.createElement("img"));
-            img.addClass("img-fluid").attr("src", item.imageUrl).attr("alt", item.name).appendTo($("#product-img"));
-            if(countArticleInBasket(item_id) > 0){
-                refreshBadgeButton(item_id);
+            if (item == null) //AUCUN ARTICLE TROUVE SUR L'ID FOURNIS, REDIRECTION DE L'UTILISATEUR
+                window.location.href = "index.html";
+            else {
+                //ARTICLE TROUVE, INSERTION DES INFORMATIONS
+                $("#product-name").html(item.name);
+                $("#product-description").html(item.description);
+                $("#product-price").html(item.price);
+                const select = $("#product-options");
+                item.colors.forEach(color => select.append(new Option(color, color)));
+                const img = $(document.createElement("img"));
+                img.addClass("img-fluid").attr("src", item.imageUrl).attr("alt", item.name).appendTo($("#product-img"));
+                if (countArticleInBasket(item_id) > 0) {
+                    refreshBadgeButton(item_id);
+                }
             }
-        }
+        });
     }).catch(function (err){
         sendAlert("danger", "<i class=\"fas fa-exclamation-triangle\"></i> Un incident lors de la connexion à l'API a eu lieu.");
     });
@@ -71,7 +72,7 @@ const showItem = (item_id) => {
 /* RAFRAICHIR LA BADGE PRESENT DANS LE BOUTON SUR LA PAGE PRODUIT */
 const refreshBadgeButton = (item_id) => {
     let badge = $(document.getElementById("button-badge"));
-    if(document.getElementById("button-badge")  == null){
+    if (document.getElementById("button-badge") == null) {
         badge = $(document.createElement("span"));
         badge.attr("id", "button-badge").addClass("mx-1 badge rounded-pill bg-secondary");
         $("#product-add").append($(badge));
@@ -81,34 +82,37 @@ const refreshBadgeButton = (item_id) => {
 
 /* AJOUT D'UN ARTICLE DANS LE PANIER */
 const addArticle = (item_id) => {
-    $.get(hostAPI+"/api/teddies").done(function(data) {
-        let item = null;
-        //RECUPERATION DE L'ARTICLE A PARTIR DE L'ID FOURNIS
-        data.forEach(itemData => {
-            if (item_id == itemData._id)
-                item = itemData;
-        });
-        if(item == null){
-            console.error("L'ID renseigné ne correspond à aucun produit.");
-        }else {
-            const basket = localStorage.getItem('basket') == null ? [] : JSON.parse(localStorage.getItem('basket'));
-            item = basket.find(item => item.id == item_id);
+    fetch(hostAPI + "/api/teddies").then(response => {
+        const responseJson = response.json(); //RECUPERATION DU JSON
+        responseJson.then(json => {
+            const data = Object.values(json); //CONVERSION DU JSON EN ARRAY
+            let item = null;
+            //RECUPERATION DE L'ARTICLE A PARTIR DE L'ID FOURNIS
+            data.forEach(itemData => {
+                if (item_id == itemData._id)
+                    item = itemData;
+            });
             if (item == null) {
-                item = {
-                    id: item_id,
-                    quantity: 1
-                };
-                basket.push(item);
+                console.error("L'ID renseigné ne correspond à aucun produit.");
             } else {
-                item.quantity += 1;
+                const basket = localStorage.getItem('basket') == null ? [] : JSON.parse(localStorage.getItem('basket'));
+                item = basket.find(item => item.id == item_id);
+                if (item == null) {
+                    item = {
+                        id: item_id,
+                        quantity: 1
+                    };
+                    basket.push(item);
+                } else {
+                    item.quantity += 1;
+                }
+                localStorage.setItem('basket', JSON.stringify(basket));
+                refreshBadgeButton(item_id);
             }
-            localStorage.setItem('basket', JSON.stringify(basket));
-            refreshBadgeButton(item_id);
-        }
+        });
     }).catch(function (err){
         console.error("Un incident lors de la connexion à l'API a eu lieu.");
     });
-
 }
 
 /* SUPPRESSION DU PANIER */
@@ -116,10 +120,10 @@ const clearBasket = () => {
     localStorage.removeItem("basket");
     //SUPPRESSION DU CONTENU DU PANIER
     const tbody = document.getElementById("panier");
-    if(tbody != null)
+    if (tbody != null)
         tbody.remove();
     $("#panier-total").html(0);
-    sendAlert("info","<i class='fas fa-info-circle'></i> Votre panier est vide");
+    sendAlert("info", "<i class='fas fa-info-circle'></i> Votre panier est vide");
 }
 
 /* QUANTITE DUN MEME ARTICLE DANS LE PANIER */
@@ -127,7 +131,7 @@ const countArticleInBasket = (item_id) => {
     let count = 0;
     const basket = localStorage.getItem('basket') == null ? [] : JSON.parse(localStorage.getItem('basket'));
     basket.forEach(item => {
-        if(item.id == item_id){
+        if (item.id == item_id) {
             count = item.quantity;
         }
     });
@@ -137,35 +141,39 @@ const countArticleInBasket = (item_id) => {
 /* AFFICHAGE DU CONTENU DU PANIER */
 const showBasket = () => {
     const basket = localStorage.getItem('basket') == null ? [] : JSON.parse(localStorage.getItem('basket'));
-    if(basket.length == 0){
+    if (basket.length == 0) {
         sendAlert("info", "<i class='fas fa-info-circle'></i> Votre panier est vide");
         document.getElementById("button_command").setAttribute("disabled", true); //Le panier est vide, on désactive le bouton commander
-    }else{
+    } else {
         const tbody = $(document.getElementById("panier"));
         let total = 0;
         basket.forEach(item => {
-            $.get(hostAPI+"/api/teddies").done(function(data){
-                data.forEach(itemData => {
-                    if(itemData._id == item.id){
-                        const tr = $(document.createElement("tr"));
-                        const articleName = $(document.createElement("td"));
-                        articleName.html("<a href='produit.html?id="+itemData._id+"'>"+itemData.name+"</a>");
-                        tr.append(articleName);
-                        const priceunit = $(document.createElement("td"));
-                        priceunit.html(itemData.price+" €");
-                        tr.append(priceunit);
-                        const quantity = $(document.createElement("td"));
-                        quantity.html(item.quantity);
-                        tr.append(quantity);
-                        const pricetotal = $(document.createElement("td"));
-                        pricetotal.html(item.quantity*itemData.price+" €");
-                        tr.append(pricetotal);
-                        tbody.append(tr);
-                        total += item.quantity*itemData.price;
-                    }
+            fetch(hostAPI + "/api/teddies").then(response => {
+                const responseJson = response.json(); //RECUPERATION DU JSON
+                responseJson.then(json => {
+                    const data = Object.values(json); //CONVERSION DU JSON EN ARRAY
+                    data.forEach(itemData => {
+                        if (itemData._id == item.id) {
+                            const tr = $(document.createElement("tr"));
+                            const articleName = $(document.createElement("td"));
+                            articleName.html("<a href='produit.html?id=" + itemData._id + "'>" + itemData.name + "</a>");
+                            tr.append(articleName);
+                            const priceunit = $(document.createElement("td"));
+                            priceunit.html(itemData.price + " €");
+                            tr.append(priceunit);
+                            const quantity = $(document.createElement("td"));
+                            quantity.html(item.quantity);
+                            tr.append(quantity);
+                            const pricetotal = $(document.createElement("td"));
+                            pricetotal.html(item.quantity * itemData.price + " €");
+                            tr.append(pricetotal);
+                            tbody.append(tr);
+                            total += item.quantity * itemData.price;
+                        }
+                    });
+                    $("#panier-total").html(total);
                 });
-                $("#panier-total").html(total);
-            }).catch(function (){
+            }).catch(function (err){
                 sendAlert("danger", "<i class=\"fas fa-exclamation-triangle\"></i> Un incident lors de la connexion à l'API a eu lieu.");
             });
         });
@@ -174,13 +182,13 @@ const showBasket = () => {
 
 /* Validation du formulaire de contact */
 const validateFormulaire = () => {
-    if(localStorage.getItem('basket') == null ){
+    if (localStorage.getItem('basket') == null) {
         sendAlert("info", "<i class='fas fa-info-circle'></i> Votre panier est vide");
-    }else{
+    } else {
         const basket = JSON.parse(localStorage.getItem('basket'));
         const productsID = [];
         basket.forEach(item => {
-            for(let i = 1; i <= item.quantity; i++) {
+            for (let i = 1; i <= item.quantity; i++) {
                 productsID.push(item.id);
             }
         });
@@ -191,25 +199,16 @@ const validateFormulaire = () => {
         const cityInput = document.getElementById("city").value; //Input Ville
         const codePostalInput = document.getElementById("code_postal").value; //Input Code postal
         //VERIFICATION QUE LES VALEURS NE SONT PAS VIDE
-        if(lastNameInput.length > 0 && firstNameInput.length > 0 && emailInput.length > 0 && addressInput.length > 0 && cityInput.length > 0 && codePostalInput.length > 0){
-            //ENVOIE DE LA REQUETE A L'API
-            let xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                //RECUPERATION DE LA REPONSE DE L'API
-                if (this.status == 201) {
-                    let data = JSON.parse(this.responseText);
-                    let price = 0;
-                    data.products.forEach(product => price += product.price);
-                    clearBasket(); //SUPPRESSION DU PANIER
-                    window.location.replace("confirm.html?orderId="+data.orderId+"&price="+price); //REDIRECTION VERS LA PAGE DE CONFIRMATION
-                }else if(this.status >= 400 && this.status < 600){ //L'API A RETOURNE UNE ERREUR
-                    sendAlert("danger", "Une erreur a eu lieu lors de l'envoie du formulaire.")
-                }
-            };
-            xhr.open("POST", hostAPI+"/api/teddies/order", true);
-            xhr.setRequestHeader('Content-Type', 'application/json');
-            //DEFINITION DU BODY
-            xhr.send(JSON.stringify({
+        if (lastNameInput.length > 0 && firstNameInput.length > 0 && emailInput.length > 0 && addressInput.length > 0 && cityInput.length > 0 && codePostalInput.length > 0) {
+
+            //DEFINITION DE LA REQUETE
+            const init = {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
                     "contact": {
                         "firstName": firstNameInput,
                         "lastName": lastNameInput,
@@ -218,9 +217,23 @@ const validateFormulaire = () => {
                         "email": emailInput,
                     },
                     "products": productsID,
-                }
-            ));
-        }else{
+                })
+            }
+
+            //ENVOIE DE LA REQUETE A L'API
+            fetch(hostAPI + "/api/teddies/order", init)
+                .then(response => {
+                    response.json().then(data => {
+                        let price = 0;
+                        data.products.forEach(product => price += product.price);
+                        clearBasket(); //SUPPRESSION DU PANIER
+                        window.location.replace("confirm.html?orderId=" + data.orderId + "&price=" + price); //REDIRECTION VERS LA PAGE DE CONFIRMATION
+                    });
+                }).catch(function (err){
+                    console.error(err);
+                sendAlert("danger", "Une erreur a eu lieu lors de l'envoie du formulaire.");
+            });
+        } else {
             sendAlert("danger", "<i class=\"fas fa-exclamation-triangle\"></i> Tous les champs sont requis.");
         }
     }
@@ -238,11 +251,11 @@ const showConfirm = () => {
 
 /* Affichage d'alerte */
 const sendAlert = (alertType, message) => {
-    if(alertType !== "primary" && alertType !== "secondary" && alertType !== "success" && alertType !== "danger" && alertType !== "warning" && alertType !== "info" && alertType !== "light" && alertType !== "dark"){
+    if (alertType !== "primary" && alertType !== "secondary" && alertType !== "success" && alertType !== "danger" && alertType !== "warning" && alertType !== "info" && alertType !== "light" && alertType !== "dark") {
         console.error("Le type d'alerte renseigné n'est pas correct.")
-    }else {
+    } else {
         const alertDiv = $(document.getElementById("alert"));
-        if(alertDiv.length > 0) {
+        if (alertDiv.length > 0) {
             alertDiv.children(0).remove();
         }
         const alert = $(document.createElement("div"));
